10c3f666503dd5de1c1d755c9cea1c4ebed9436e
tshinohara <takuma1201@gmail.com>
2024-02-05 16:22:23 +0000

    jihoのTS対応

--- src/jiho/jiho.ts
+++ src/jiho/jiho.ts
@@ -2,19 +2,19 @@ import moment from "moment-timezone";
 import BskyUtils from "../common/bsky.js";
 
 export default class jiho {
-  bsky = null;
-  constructor(author, pass) {
+  bsky: BskyUtils;
+  constructor(author: string, pass: string) {
     this.bsky = new BskyUtils("https://bsky.social", author, pass);
   }
 
   process = async () => {
     const waitTime = Math.floor(Math.random() * 360000); // 0~300000ミリ秒（5分）のランダムな値
     setTimeout(async () => {
-      console.log(waitTime)
+      console.log(waitTime);
       const time = moment().tz("Asia/Tokyo").format("YYYY/MM/DD HH:mm:ss");
       //   post("なう(" + time + ")");
       await this.bsky.login();
-      await this.bsky.post("なう(" + time + ")");
+      await this.bsky.post(`なう(${time})`);
     }, waitTime);
   };
 }
