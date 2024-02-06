c5fd980d17d0061f2cc8c98000fcf9352b945a9f
tshinohara <takuma1201@gmail.com>
2024-02-06 20:05:38 +0900

    cron.ts のTS対応

--- src/cron.ts
+++ src/cron.ts
@@ -1,19 +1,19 @@
-import * as cron from "node-cron";
 import dotenv from "dotenv";
+import * as cron from "node-cron";
+import jiho from "./jiho/jiho.js";
+import system from "./system/system.js";
 import wnlive from "./wnlive-schedule/wnlive.js";
-import jiho from "./jiho/jiho.js"
-import system from "./system/system.js"
 
 dotenv.config();
 const { JIHO_AUTHOR, JIHO_PASSWORD } = process.env;
 const { WNLIVE_AUTHOR, WNLIVE_PASSWORD } = process.env;
 const { SYSTEM_AUTHOR, SYSTEM_PASSWORD } = process.env;
 
-let wn = new wnlive(WNLIVE_AUTHOR, WNLIVE_PASSWORD);
-let jh = new jiho(JIHO_AUTHOR, JIHO_PASSWORD);
+const wn = new wnlive(WNLIVE_AUTHOR ?? "", WNLIVE_PASSWORD ?? "");
+const jh = new jiho(JIHO_AUTHOR ?? "", JIHO_PASSWORD ?? "");
 
 // システム起動通知
-let sys = new system(SYSTEM_AUTHOR, SYSTEM_PASSWORD);
+const sys = new system(SYSTEM_AUTHOR ?? "", SYSTEM_PASSWORD ?? "");
 sys.process();
 
 cron.schedule("45 18 * * *", wn.process);
